2d49a4f379f1549a4d27f0c23e2a254e3cf421b5
Madhav1217 <madhava1219@gmail.com>
2017-05-25 09:08:53 +0530

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-echo "enter the a value"
-read a
-echo "enter the b value"
-read b
 echo "select the option \n 1.add \n 2.sub \n 3.mul \n 4.div"
 echo "select the choice no"
-read choice
+a=$1
+b=$2
+choice=$3
 case "$choice" in 
 1)add=`expr $a + $b`
 echo addtion=$add;;
